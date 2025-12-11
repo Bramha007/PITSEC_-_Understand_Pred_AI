77ce03580ac1f480ac3650b0a0ad1216425aa930
Nitish <nitish.chy.007@gmail.com>
2025-12-11 16:39:33 +0100

    path fixes

--- New-approach/src/setup/config_det.py
+++ New-approach/src/setup/config_det.py
@@ -22,8 +22,8 @@ XML_DIR_ALL   = os.path.join(DATA_ROOT, "annotations")
 
 # Testing paths for the RECTANGLES dataset (Used for cross-domain testing)
 # You MUST change the path below to point to your 'sized_rectangles_filled' location
-RECT_DATA_ROOT = r'E:\WPT-Project\Data\sized_rectangles_filled' 
-# RECT_DATA_ROOT = "/pitsec_sose2025_team3_1/data/sized_rectangles_filled" # for Linux
+# RECT_DATA_ROOT = r'E:\WPT-Project\Data\sized_rectangles_filled' 
+RECT_DATA_ROOT = "/pitsec_sose2025_team3_1/data/sized_rectangles_filled" # for Linux
 IMG_DIR_TEST_RECT  = os.path.join(RECT_DATA_ROOT, 'test')
 XML_DIR_ALL_RECT   = os.path.join(RECT_DATA_ROOT, 'annotations')
 
